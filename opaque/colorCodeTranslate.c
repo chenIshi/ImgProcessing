098ddfa4f38cc544753bd8e533e657355e0ada71
chenIshi <chen.ishi@gmail.com>
2018-03-09 09:16:40 +0800

    modify test file

--- opaque/colorCodeTranslate.c
+++ opaque/colorCodeTranslate.c
@@ -12,31 +12,35 @@ struct rgb_color {
 };
 
 struct cmyk_color {
-    int c;
-    int m;
-    int y;
-    int k;
+    double c;
+    double m;
+    double y;
+    double k;
 };
 
 //From RGB
 //To CMYK
-struct cmyk_color RGBtoCMYK(int rgb_color_r, int rgb_color_g, int rgb_color_b){
+void RGBtoCMYK(int rgb_color_r, int rgb_color_g, int rgb_color_b, double *ret_c, double *ret_m, double *ret_y, double *ret_k){
     struct cmyk_color ret_val;
 
     //formula from Adobe https://forums.adobe.com/thread/428899
     //RGB to CMY
-    ret_val.c = 1-(rgb_color_r/255);
-    ret_val.m = 1-(rgb_color_g/255);
-    ret_val.y = 1-(rgb_color_b/255);
+
+    ret_val.c = 1-(rgb_color_r/255.0);
+    ret_val.m = 1-(rgb_color_g/255.0);
+    ret_val.y = 1-(rgb_color_b/255.0);
 
     //CMY to CMYK
     //k indicates "black"
     ret_val.k = 1;
     
     //side effect trial
+
     (ret_val.c < ret_val.k) && (ret_val.k = ret_val.c);
     (ret_val.m < ret_val.k) && (ret_val.k = ret_val.m);
     (ret_val.y < ret_val.k) && (ret_val.k = ret_val.y);
+
+    printf("During Fuction call: C: %lf, M: %lf, Y: %lf, K: %lf\n", ret_val.c, ret_val.m, ret_val.y, ret_val.k);
 
     if(ret_val.k == 1){
         ret_val.c = ret_val.m = ret_val.y = 0;
@@ -46,5 +50,11 @@ struct cmyk_color RGBtoCMYK(int rgb_color_r, int rgb_color_g, int rgb_color_b){
         ret_val.y = (ret_val.y - ret_val.k)/(1-ret_val.k);
     }
 
-    return ret_val;
+    ret_c = &(ret_val.c);
+    ret_m = &(ret_val.m);
+    ret_y = &(ret_val.y);
+    ret_k = &(ret_val.k);
+
+    printf("End of the function call: ret_c: %lf, ret_m: %lf, ret_y: %lf, ret_k: %lf\n", *ret_c, *ret_m, *ret_y, *ret_k);
+
 }
--- opaque/test.c
+++ opaque/test.c
@@ -2,5 +2,22 @@
 #include "colorCodeTranslate.c"
 
 int main(){
-    RGBtoCMYK(28,28,28);
+    double c, m, y, k;
+    double *pc = &c;
+    double *pm = &m;
+    double *py = &y;
+    double *pk = &k;
+    /*
+    int a = 1;
+    int b = 2;
+    int p = 3;
+    int d = 4;
+    c= &a;
+    m= &b;
+    y= &p;
+    k= &d;
+    */
+    RGBtoCMYK(28,28,28, pc, pm, py, pk);
+    printf("C: %lf, M: %lf, Y: %lf, K: %lf\n", c, m, y, k);
+    return 0;
 }
